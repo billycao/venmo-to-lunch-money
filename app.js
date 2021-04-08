cbd7647a1dd9259a64c2aa5d43f805f54b5f21ef
Billy Cao <the.billy.c@gmail.com>
2021-04-07 23:18:27 -0700

    Refactor logic into VenmoEmail obj, and add more expressive configs.

--- app.js
+++ app.js
@@ -2,110 +2,65 @@ const config = require('config');
 const express = require('express');
 const formidable = require('formidable');
 const fs = require('fs');
-const htmlParser = require('node-html-parser');
 const LunchMoney = require('lunch-money').default;
+const VenmoEmail = require('./venmo-lib');
 
 const app = express();
 
 const lunchMoney = new LunchMoney({ token: config.get('lunch-money.api-token') });
 
+function sendTransaction(draftTransaction) {
+  lunchMoney.createTransactions({
+    transactions: [draftTransaction],
+    applyRules: config.get('venmo-email.apply-rules'),
+    checkForRecurring: config.get('venmo-email.check-for-recurring'),
+    debitAsNegative: false
+  })
+  .then(
+    (res) => { console.log(res) },
+    (err) => { console.error(err) }
+  );
+}
+
 // We provide a GET endpoint to play nice with mailin.
-app.get('/incoming-emails', (req, res) => {
+httpEndpoint = config.get('express.http-endpoint');
+app.get(httpEndpoint, (req, res) => {
   res.end();
 });
 
-app.post('/incoming-emails', (req, res, next) => {
+app.post(httpEndpoint, (req, res, next) => {
   const form = formidable();
 
   form.parse(req, (err, fields, files) => {
     if (err) {
+      console.error('Received invalid form contents. Skipping.');
       next(err);
       return;
     }
-    emailTxt = fields['mailinMsg'];
-    emailJSON = JSON.parse(emailTxt);
-    handleVenmoEmail(emailJSON);
 
+    if ('mailinMsg' in fields) {
+      let venmoEmail = new VenmoEmail(JSON.parse(fields['mailinMsg']));
+      draftTransaction = venmoEmail.getDraftTransaction();
+      if (draftTransaction) {
+        console.log('Sending transaction to Lunch Money:');
+        console.log(draftTransaction);
+        sendTransaction(draftTransaction);
+      } else {
+        console.log('Could not create draftTransaction from email. Skipping...');
+      }
+    } else {
+      console.error('field "mailinMsg" not found in form contents. Skipping.');
+    }
     res.end();
   });
 });
 
-const port = 3001
-app.listen(port);
+// const port = process.env.EXPRESS_PORT || config.get('express.default-http-port');
+// const host = config.get('express.http-host');
+// console.log(`Listening for new emails POSTed by mailin to http://${host}:${port}${httpEndpoint}`);
+// app.listen(port);
 
-// let emailTxt = fs.readFileSync('email.txt', 'utf8');
-// let emailJSON = JSON.parse(emailTxt);
-// handleVenmoEmail(emailJSON);
-
-function handleVenmoEmail(emailJSON) {
-  let subject = emailJSON['headers']['subject'];
-
-  let idMatch = emailJSON['html'].match(/Payment\ ID:\ (?<id>\d+)/);
-  if (idMatch) {
-    id = idMatch['groups']['id'];
-  } else {
-    console.error(`Could not find Payment ID for "${subject}". Skipping.`);
-    return;
-  }
-
-  let date = emailJSON['date'];
-  let dateMatch = emailJSON['html'].match(/(?<date>[A-Z][a-z][a-z]\ [0-9][0-9],\ [0-9][0-9][0-9][0-9])\ PDT/);
-  if (dateMatch) {
-    dateObj = new Date(Date.parse(dateMatch['groups']['date']));
-    date = dateObj.toISOString();
-  }
-
-  let payee = '';
-  let amount = '';
-  let match = null;
-  let sign = ''
-  if (subject.includes('You paid')) {
-    // "You paid Jane Doe $5.00"
-    match = subject.match(/You\ paid (?<recipient>.+)\ \$(?<amount>.+)/);
-  } else if (subject.includes('You completed')) {
-    // "You completed Jane Doe's $28.00 charge request"
-    match = subject.match(/You\ completed (?<recipient>.+)'s\ \$(?<amount>.+)\ c.+/);
-  } else if (subject.includes('paid your')) {
-    // "Jane Doe paid your $47.20 request"
-    match = subject.match(/(?<recipient>.+)\ paid\ your\ \$(?<amount>.+)\ request/);
-    sign = '-';
-  } else if (subject.includes('paid you')) {
-    // "Jane Doe paid you $5.00"
-    match = subject.match(/(?<recipient>.+)\ paid\ you\ \$(?<amount>.+)/);
-    sign = '-';
-  } else {
-    console.error(`Invalid subject: ${subject}. Skipping transaction creation.`);
-    return;
-  }
-  payee = match['groups']['recipient'];
-  amount = sign + match['groups']['amount'];
-
-  let htmlRoot = htmlParser.parse(emailJSON['html']);
-  let memo = htmlRoot.querySelectorAll('table > tbody > tr:first-child > td:nth-child(2) > div:nth-child(2) > p')[0].text;
-
-  recordVenmoPayment(id, date, payee + ' - ' + memo, amount);
-}
-
-function recordVenmoPayment(id, date, payee, amount) {
-  draftTransaction = {
-    external_id: id,
-    date: date,
-    payee: payee,
-    amount: amount,
-    currency: 'usd',
-    notes: 'VenmoEmail',
-    status: 'uncleared',
-    is_group: false,
-  };
-  console.log('Sending transaction to Lunch Money:');
-  console.log(draftTransaction);
-  lunchMoney.createTransactions(
-    [draftTransaction],
-    false,
-    false,
-    false)
-  .then(
-    (res) => { console.log(res) },
-    (err) => { console.error(err) });
-}
-
+const email = JSON.parse(fs.readFileSync('email.txt'));
+let venmoEmail = new VenmoEmail(email);
+console.log(venmoEmail.getDraftTransaction());
+sendTransaction(venmoEmail.getDraftTransaction())
---
+++ venmo-lib.js
@@ -0,0 +1,121 @@
+const config = require('config');
+const htmlParser = require('node-html-parser');
+
+class VenmoEmail {
+  emailJSON;
+
+  dateRegex;
+  idRegex;
+  subjectDebitRegexes;
+  subjectCreditRegexes;
+  memoCSSSelector;
+
+  defaultCurrency;
+  defaultNote;
+  defaultState;
+
+  constructor(emailJSON) {
+    this.emailJSON = emailJSON;
+    // TODO(billycao): Validate POST or JSON contents.
+    // TODO(billycao): Honestly we should just use Typescript for this...
+
+    this.idRegex = new RegExp(config.get('venmo-email.id-regex'));
+    this.dateRegex = new RegExp(config.get('venmo-email.date-regex'));
+
+    this.subjectDebitRegexes = [];
+    for (let regexStr of config.get('venmo-email.subject-debit-regexes')) {
+      this.subjectDebitRegexes.push(new RegExp(regexStr));
+    }
+    this.subjectCreditRegexes = [];
+    for (let regexStr of config.get('venmo-email.subject-credit-regexes')) {
+      this.subjectCreditRegexes.push(new RegExp(regexStr));
+    }
+
+    this.memoCSSSelector = config.get('venmo-email.memo-css-selector');
+
+    this.defaultCurrency = config.get('venmo-email.currency');
+    this.defaultNote = config.get('venmo-email.note');
+    this.defaultState = config.get('venmo-email.state');
+  }
+
+  getSubject() {
+    return this.emailJSON['headers']['subject'];
+  }
+
+  getPayeeAndAmountFromSubject() {
+    let subject = this.getSubject();
+    for (let regex of this.subjectDebitRegexes) {
+      let match = subject.match(regex);
+      if (match) {
+        return [match['groups']['payee'], match['groups']['amount']];
+      }
+    }
+    for (let regex of this.subjectCreditRegexes) {
+      let match = subject.match(regex);
+      if (match) {
+        return [match['groups']['payee'], '-' + match['groups']['amount']];
+      }
+    }
+    return;
+  }
+
+  getPaymentID() {
+    let idMatch = this.emailJSON['html'].match(this.idRegex);
+    if (idMatch) {
+      if ('groups' in idMatch && 'id' in idMatch['groups']) {
+        return idMatch['groups']['id'];
+      }
+      console.error('Missing named capture group "id". Is id-regex formatted correctly?');
+      return;
+    }
+    return;
+  }
+
+  getPaymentDateAsISOString() {
+    let dateMatch = this.emailJSON['html'].match(this.dateRegex);
+    if (dateMatch) {
+      let dateObj = new Date(Date.parse(dateMatch['groups']['date']));
+      return dateObj.toISOString();
+    }
+    console.warn('Could not extract payment date from email body. Falling back to email date.');
+    return this.emailJSON['date'];
+  }
+
+  getPaymentMemo() {
+    let htmlRoot = htmlParser.parse(this.emailJSON['html']);
+    let memoElems = htmlRoot.querySelectorAll(this.memoCSSSelector);
+    if (!memoElems.length) {
+      console.error('Could not extract memo from email. Is memo-css-selector correct?');
+      return
+    }
+    return memoElems[0].text;
+  }
+
+  getDraftTransaction() {
+    let subject = this.getSubject();
+    let [payee, amount] = this.getPayeeAndAmountFromSubject();
+    if (!(payee && amount)) {
+      console.log(`Unrecognized email subject: ${subject}`);
+      return;
+    }
+
+    let id = this.getPaymentID();
+    let date = this.getPaymentDateAsISOString();
+    let memo = this.getPaymentMemo();
+    if  (!(id && date && memo)) return;
+
+    return {
+      external_id: id,
+      date: date,
+      payee: payee + ' - ' + memo,
+      amount: amount,
+      currency: this.defaultCurrency,
+      notes: this.defaultNote,
+      status: this.defaultState,
+    };
+  }
+
+
+}
+
+module.exports = VenmoEmail;
