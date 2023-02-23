f6c427cebb1ac835af56a55c4da695299affb0aa
Billy Cao <the.billy.c@gmail.com>
2023-02-23 14:23:48 -0800

    Add unit test for receiving money

--- node/test/test.ts
+++ node/test/test.ts
@@ -3,38 +3,62 @@ const fs = require('fs');
 const path = require('path');
 
 const assert = require('assert').strict;
+const expect = require('chai').expect;
 
 const app = require('../built/app');
 const VenmoEmail = require('../built/venmo-lib');
 
 import { ParsedMail, HeaderValue, HeaderLines } from 'mailparser';
 
-describe('VenmoEmail', function() {
-  it('should parse sent money emails', function() {
-    let emailHTML = fs.readFileSync(
-      path.join(__dirname, '../testdata/venmo-send-money-email.txt'),
+function getEmailFromTestdata(
+  subject: string,
+  date: string,
+  emailBodyRelativePath: string): ParsedMail {
+   let emailHTML = fs.readFileSync(
+      path.join(__dirname, emailBodyRelativePath),
       {'encoding': 'utf-8'});
-    let emailDate = new Date('2023-02-12T16:29:00.000Z');
+    let emailDate = new Date(date);
 
-    let email: ParsedMail = {
+    return {
       'attachments': [],
       'headers': new Map<string, HeaderValue>(),
       'headerLines': [],
-      'subject': 'You paid Angela Lau $10.00',
+      'subject': subject,
       'date': emailDate,
       'html': '',
       'text': emailHTML,
     };
+}
 
-    let venmoEmail = new VenmoEmail(email, app.getOptionsFromConfig());
+describe('VenmoEmail', function() {
+  it('should parse sent money emails', function() {
+    let email = getEmailFromTestdata(
+      'You paid Jane Doe $10.00',
+      '2023-02-12T16:29:00.000Z',
+      '../testdata/venmo-send-money-email.txt');
+    let venmoEmail = new VenmoEmail(email,  app.getOptionsFromConfig());
     let draftTransaction = venmoEmail.getDraftTransaction();
-
-    console.log(draftTransaction);
 
     assert.equal(venmoEmail.getPaymentID(), '3736958406176898632');
     assert.equal(draftTransaction.date, '2023-02-12T00:00:00.000Z');
     assert.equal(draftTransaction.amount, '10.00');
-    assert(draftTransaction.payee.includes('Angela Lau'));
-    assert(draftTransaction.payee.includes('Gas for rental car'));
+    expect(draftTransaction.payee).to.include('Jane Doe');
+    expect(draftTransaction.payee).to.include('Gas for rental car');
+  });
+
+  it('should parse receive money emails', function() {
+    let email = getEmailFromTestdata(
+      'Bob Smith paid you $250.00',
+      '2023-02-21T06:13:00.000Z',
+      '../testdata/venmo-receive-money-email.txt');
+    let venmoEmail = new VenmoEmail(email,  app.getOptionsFromConfig());
+    let draftTransaction = venmoEmail.getDraftTransaction();
+
+    assert.equal(venmoEmail.getPaymentID(), '3720437832885961799');
+    assert.equal(draftTransaction.date, '2023-01-20T00:00:00.000Z');
+    assert.equal(draftTransaction.amount, '-250.00');
+
+    expect(draftTransaction.payee).to.include('Bob Smith');
+    expect(draftTransaction.payee).to.include('Hotel');
   });
 });
